4ca1d9eb0528e5e568fa8ea9165d2056460f4bcb
Fbarrera411 <freddy.barrera.a@gmail.com>
2024-08-10 09:33:24 -0400

    primer commit

---
+++ app-cdk/lib/app-cdk-stack.ts
@@ -0,0 +1,17 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class AppCdkStack extends cdk.Stack {
+
+  public readonly repository: ecr.Repository;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    
+    this.repository = new ecr.Repository(this, 'my_app');
+
+  }
+}
---
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -0,0 +1,163 @@
+import * as cdk from 'aws-cdk-lib';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
+
+
+
+import { Construct } from 'constructs';
+
+interface ConsumerProps extends StackProps {
+  ecrRepository: ecr.Repository,
+}
+
+export class MyPipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: ConsumerProps) {
+    super(scope, id, props);
+
+    // Recupera el secreto de GitHub
+    const githubSecret = secretsmanager.Secret.fromSecretNameV2(this, 'GitHubSecret', 'github/personal_access_token');
+
+    // Crea un proyecto de CodeBuild
+    const buildProject = new codebuild.PipelineProject(this, 'BuildProject', {
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
+        privileged: true,
+        computeType: codebuild.ComputeType.LARGE,
+      },
+      buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec.yml'),
+    });
+
+    // Define los artefactos
+    const sourceOutput = new codepipeline.Artifact();
+    const buildOutput = new codepipeline.Artifact();
+    const unitTestOutput = new codepipeline.Artifact();
+
+    // Define el pipeline
+    const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
+      pipelineName: 'CICD_Pipeline',
+      crossAccountKeys: false,
+    });
+
+    const dockerBuild = new codebuild.PipelineProject(this, 'DockerBuild', {
+      environmentVariables: {
+        IMAGE_TAG: { value: 'latest' },
+        IMAGE_REPO_URI: { value: props.ecrRepository.repositoryUri },
+        AWS_DEFAULT_REGION: { value: process.env.CDK_DEFAULT_REGION },
+      },
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
+        privileged: true,
+        computeType: codebuild.ComputeType.LARGE,
+      },
+      buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec_docker.yml'),
+    });
+
+    const dockerBuildRolePolicy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: ['*'],
+      actions: [
+        'ecr:GetAuthorizationToken',
+        'ecr:BatchCheckLayerAvailability',
+        'ecr:GetDownloadUrlForLayer',
+        'ecr:GetRepositoryPolicy',
+        'ecr:DescribeRepositories',
+        'ecr:ListImages',
+        'ecr:DescribeImages',
+        'ecr:BatchGetImage',
+        'ecr:InitiateLayerUpload',
+        'ecr:UploadLayerPart',
+        'ecr:CompleteLayerUpload',
+        'ecr:PutImage',
+      ],
+    });
+
+    dockerBuild.addToRolePolicy(dockerBuildRolePolicy);
+
+    const dockerBuildOutput = new codepipeline.Artifact();
+
+    const signerARNParameter = new ssm.StringParameter(this, 'SignerARNParam', {
+      parameterName: 'signer-profile-arn',
+      stringValue: 'arn:aws:signer:us-east-2:767397769903:/signing-profiles/ecr_signing_profile',
+    });
+
+    const signerParameterPolicy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: [signerARNParameter.parameterArn],
+      actions: ['ssm:GetParametersByPath', 'ssm:GetParameters'],
+    });
+
+    const signerPolicy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: ['*'],
+      actions: [
+        'signer:PutSigningProfile',
+        'signer:SignPayload',
+        'signer:GetRevocationStatus',
+      ],
+    });
+
+    dockerBuild.addToRolePolicy(signerParameterPolicy);
+    dockerBuild.addToRolePolicy(signerPolicy);
+
+
+    // Agrega la etapa de origen con GitHub
+    pipeline.addStage({
+      stageName: 'Source',
+      actions: [
+        new codepipeline_actions.GitHubSourceAction({
+          actionName: 'GitHub_Source',
+          owner: 'Fbarrera411',
+          repo: 'AWS_CI_CD_Workshop',
+          branch: 'main', // o la rama que prefieras
+          oauthToken: githubSecret.secretValue,
+          output: sourceOutput,
+        }),
+      ],
+    });
+
+    //Agrega la etapa de tetsing
+    pipeline.addStage({
+      stageName: 'Code-Quality-Testing',
+      actions: [
+        new codepipeline_actions.CodeBuildAction({
+          actionName: 'Unit-Test',
+          project: buildProject,
+          input: sourceOutput,
+          outputs: [unitTestOutput],
+        }),
+      ],
+    });
+
+    pipeline.addStage({
+      stageName: 'Docker-Push-ECR',
+      actions: [
+        new codepipeline_actions.CodeBuildAction({
+          actionName: 'Docker-Build',
+          project: dockerBuild,
+          input: sourceOutput,
+          outputs: [dockerBuildOutput],
+        }),
+      ],
+    });
+
+
+    // Agrega la etapa de construcción
+    pipeline.addStage({
+      stageName: 'Build',
+      actions: [
+        new codepipeline_actions.CodeBuildAction({
+          actionName: 'Build',
+          project: buildProject,
+          input: sourceOutput,
+          outputs: [buildOutput],
+        }),
+      ],
+    });
+  }
+}
